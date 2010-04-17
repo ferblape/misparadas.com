f1c110112103ec9948f865e28717bb53752dfc33
Fernando Blat Peris <ferblape@gmail.com>
2010-04-17 23:47:28 +0200

    Parsing addresses in order to add Madrid if the address doesn't not include the city name

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -6,11 +6,22 @@ class BusStopsController < ApplicationController
       @bus_stop = BusStop.find_by_emt_id(params[:code].strip)
       render @bus_stop
     elsif params[:address]
-      @bus_stops = BusStop.find(:all, :origin => params[:address].strip, :within => 0.2, :order => 'distance ASC')
+      @bus_stops = BusStop.find(:all, :origin => parsed_address(params[:address]), :within => 0.2, :order => 'distance ASC')
     end
   end
 
   def show
+  end
+  
+  private
+  
+  def parsed_address(address)
+    address.strip!
+    if address =~ /(.*)\s*,\s*Madrid\s*/i
+      address
+    else
+      "#{address}, Madrid"
+    end
   end
   
 end
