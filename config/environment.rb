c13ab6b793dac310445eb38027c7d56820b5e33f
Christos Zisopoulos <christos@42linesofcode.com>
2010-05-01 12:19:48 +0200

    Vendor rack 1.0.1 (a Rails 2.3.5 dep)

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "hpricot"
   config.gem "geokit"
   config.gem "haml"
+  config.gem "rack", "~>1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
