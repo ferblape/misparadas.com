f661dd398968666c642d2459f7901558dd69b0b5
Christos Zisopoulos <christos@42linesofcode.com>
2010-05-02 00:08:04 +0200

    Fifteen minutes of fame

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
       :width => wd, :height => ht)
   end
 
+  def fifteen_minutes_of_fame
+    [
+      '<a href="http://fernando.blat.es/">Fernando Blat</a>',
+      '<a href="http://samlown.com">Sam Lown</a>',
+      '<a href="http://blog.duopixel.com/">Mark MacKay</a>',
+      '<a href="http://42linesofcode.com/">Christos Zisopoulos</a>'
+    ].sort_by{rand}.to_sentence(:last_word_connector => ' y ')
+  end
 end
